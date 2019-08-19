7718d9b7eb2dc4c7b8e8862e2971586ff37eb8b7
Gavin Macaulay <gavin@macaulay.co.nz>
2019-08-19 16:08:08 +0200

    Add sound speed

--- demo_mask.m
+++ demo_mask.m
@@ -13,9 +13,12 @@ et = h5read(f, '/Interpretation/end_time');
 id = h5read(f, '/Interpretation/id');
 name = h5read(f, '/Interpretation/name');
 
+c = h5read(f, '/Interpretation/sound_speed');
+
 % and some attributes
 time_units = h5readatt(f, '/Interpretation/mask_times', 'units');
 depth_units = h5readatt(f, '/Interpretation/mask_depths', 'units');
+c_units = h5readatt(f, '/Interpretation/sound_speed', 'units');
 
 clf
 % plot each bounding box and the ping masks inside the boxes
@@ -33,6 +36,8 @@ for i = 1:length(t)
         plot([tt(j) tt(j)], [dd(j,1) dd(j,2)], 'LineWidth', 2)
     end
 end
+
+title(['Regions (c=' num2str(c) ' [' c_units '])'])
 
 set(gca, 'Ydir', 'reverse')
 xlabel(['Time (' time_units ')'])
