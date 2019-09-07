3352cf88d71a27ec4e0c7517578fb04a8172162c
gavinmacaulay <gavin@macaulay.co.nz>
2019-09-07 17:49:59 +0200

    Remove casting to np.array (wasn't necessary)

--- demo_mask_vlen.py
+++ demo_mask_vlen.py
@@ -6,7 +6,6 @@ Created on Tue Aug 20 11:44:06 2019
 """
 
 import h5py
-import numpy as np
 import matplotlib.pyplot as plt
 
 #Name of the netcdf file
@@ -18,8 +17,8 @@ f = h5py.File(filename, 'r')
 interp = f['Interpretation/v1']
 
 # Get some variables and attributes
-t = np.array(interp['mask_times'])
-d = np.array(interp['mask_depths'])
+t = interp['mask_times']
+d = interp['mask_depths']
 
 d_units = str(interp['mask_depths'].attrs['units'], 'utf-8')
 
@@ -29,21 +28,19 @@ t_calendar = str(interp['mask_times'].attrs['calendar'], 'utf-8')
 c = interp['sound_speed'][()]
 c_units = str(interp['sound_speed'].attrs['units'], 'utf-8')
 
-bb_upper = np.array(interp['min_depth'])
-bb_lower = np.array(interp['max_depth'])
-bb_left = np.array(interp['start_time'])
-bb_right = np.array(interp['end_time'])
+bb_upper = interp['min_depth']
+bb_lower = interp['max_depth']
+bb_left = interp['start_time']
+bb_right = interp['end_time']
 
-region_id = np.array(interp['id'])
-region_name = np.array(interp['name'])
-r_type = np.array(interp['region_type'])
+region_id = interp['id']
+region_name = interp['name']
+r_type = interp['region_type']
 r_type_enum = h5py.check_dtype(enum=interp['region_type'].dtype)
 # Convert region types into a text version
 r_type_enum = dict(map(reversed, r_type_enum.items()))
 r_type_name = [r_type_enum[i] for i in r_type]
 
-#close dataset
-f.close()
 
 #Plot the power of beam
 plt.figure(1)
@@ -52,14 +49,13 @@ plt.clf()
 # plot masks
 for i, r in enumerate(d):
     for time, ranges in zip(t[i], r):
-        print(time)
         for start, stop in zip(ranges[0::2], ranges[1::2]):
-            plt.plot(np.array([time, time]), np.array([start, stop]), linewidth=4, color='k')
+            plt.plot([time, time], [start, stop], linewidth=4, color='k')
 
 # plot bounding boxes
 for i, junk in enumerate(bb_upper):
-    plt.plot(np.array([bb_left[i], bb_right[i], bb_right[i], bb_left[i], bb_left[i]]), 
-             np.array([bb_lower[i], bb_lower[i], bb_upper[i], bb_upper[i], bb_lower[i]]),
+    plt.plot([bb_left[i], bb_right[i], bb_right[i], bb_left[i], bb_left[i]], 
+             [bb_lower[i], bb_lower[i], bb_upper[i], bb_upper[i], bb_lower[i]],
              color=(0.5, 0.5, 0.5))
     plt.text(bb_left[i], bb_upper[i], 'ID: ' + str(region_id[i]) 
             + ' (' + r_type_name[i] + ')', 
@@ -70,5 +66,9 @@ ax.invert_yaxis()
 
 plt.xlabel('Time\n(' + t_units + ')')
 plt.ylabel('Depth (' + d_units + ')')
+
+#close dataset
+f.close()
+
 
 plt.savefig('demo_mask_vlen.png', bbox_inches='tight')
