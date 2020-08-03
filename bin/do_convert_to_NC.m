8d339f139c08b2852e02146bb5bcaa0c096f381d
Nils Olav Handegard <nils.olav.handegard@imr.no>
2020-08-03 22:27:18 +0200

    Changed to follow the IMR folder structure

--- bin/do_convert_to_NC.m
+++ bin/do_convert_to_NC.m
@@ -1,3 +1,22 @@
+%% User parameters
+%Path to ncgen
+str0 = ['"C:\Program Files\netCDF 4.7.4\bin\ncgen" -b '];
+
+% Path to the example data
+%whr = which('LSSSreader_readsnapfiles');
+%[dr,~,~] = fileparts(whr);
+%dr = dr(1:end-3);
+dr = 'D:\DATA\LSSS-label-versioning';
+
+% Recursively list relevant data in the example directory. This can also be
+% used to search files in any folder structure
+files.snap = rdir(fullfile(dr,'**','*.snap'));
+files.work = rdir(fullfile(dr,'**','*.work'));
+files.raw = rdir(fullfile(dr,'**','*.raw'));
+
+% Match the corresponding snap, work and raw files (by file name)
+files = LSSSreader_pairfiles(files);
+
 %% Initialize
 
 dat.date_created = '20190819T134900Z';
@@ -11,8 +30,6 @@ dat.keywords = 'scrutinisation mask, echosounder';
 dat.summary = 'Contains definitions of echogram scrutiny masks';
 dat.title = 'Echogram scrutiny masks';
 
-%Path to ncgen
-str0 = ['"C:\Program Files\netCDF 4.7.4\bin\ncgen" -b '];
 
 % group: Interpretation {
 %     group: v1 { // subsequent versions of this interpretation get put in new subgroups, using the numbering system v1, v2, etc.
@@ -37,36 +54,24 @@ dat.group(1).region_comment = "";
 
 dat.group(1).region_type = "analysis";
 
-% Path to the example data
-whr = which('LSSSreader_readsnapfiles');
-[dr,~,~] = fileparts(whr);
-dr = dr(1:end-3);
 
-% Recursively list relevant data in the example directory. This can also be
-% used to search files in any folder structure
-files.snap = rdir(fullfile(dr,'exampledata','**','*.snap'));
-files.work = rdir(fullfile(dr,'exampledata','**','*.work'));
-files.raw = rdir(fullfile(dr,'exampledata','**','*.raw'));
-
-% Match the corresponding snap, work and raw files (by file name)
-files = LSSSreader_pairfiles(files);
 
 pl = true; % Set to false for plotting the masks only (without background echograms)
 %pl = false;
-
-for file=[1:7 9:size(files.F,1)]%LSSSreader fails on file 8. Blame Gavin.
+%%
+for file=12:13%:size(files.F,1)
     snap = files.F{file,1};
     work = files.F{file,2};
     raw  = files.F{file,3};
     % If no snap files are present, use the work files instead
-    if isempty(snap)
-        snap=work;
+    if isempty(work)
+        work=snap;
     end
     
-    NCfile = [snap(1:end-5),'.cdl'];
+    NCfile = [work(1:end-5),'.cdl'];
     disp(NCfile)
     % Create cdl file
-    numRegions = convertWorkToNC(snap, raw, NCfile, 38000,dat);
+    numRegions = convertWorkToNC(work, raw, NCfile, 38000,dat);
     % Create NC file
     str = [str0,NCfile];
     system(str);
